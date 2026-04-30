c8f0fadf891e4769398325dd8766121f395c5212
bc-ania <anna.jezierska@bigcommerce.com>
2026-04-30 10:25:51 +0200

    feat(payment): PI-5142 Replace ; with ,

--- packages/core/src/app/payment/Payment.tsx
+++ packages/core/src/app/payment/Payment.tsx
@@ -764,7 +764,7 @@ export function mapToPaymentProps({
     }
 
     const checkoutSettings = config.checkoutSettings & {
-        orderTermsAndConditionsLocation: string;
+        orderTermsAndConditionsLocation: string,
     };
 
     const {
